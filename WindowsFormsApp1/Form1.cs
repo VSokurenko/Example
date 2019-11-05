e8568c2372716b0d18edf897315f0997875857b3
V. Sokurenko <sokurenko2@meta.ua>
2019-11-05 14:38:01 +0200

    fdfdfd

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -15,7 +15,7 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
-            Text = "Hello2";
+            Text = "Hello222555";
         }
 
     }
